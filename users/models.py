27b2d8a67006dcce6f7b56c4d022d4e1fe38df62
haochengz <haochengzhao@outlook.com>
2018-04-16 11:49:46 +0800

    测试UserProfile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=20, verbose_name="昵称", default="")
+    birthday = models.DateField(verbose_name="生日", null=True, blank=True)
+    gender = models.CharField(verbose_name="性别", choices=(("male", "男"), ("female", "女")), default="male", max_length=6)
+    address = models.CharField(verbose_name="地址", max_length=100, default="")
+    mobile = models.CharField(verbose_name="手机号码", max_length=11, null=True, blank=True)
+    image = models.ImageField(verbose_name="头像", upload_to="media/image/%Y/%m", default="media/image/default.png")
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from django.utils import timezone
+from datetime import datetime
 
-# Create your tests here.
+from .models import UserProfile
+
+
+class UserProfileTest(TestCase):
+
+    def test_userprofile_saving_and_retrieving_from_db(self):
+        john = UserProfile(
+            username="john",
+            password="123456",
+            first_name="John",
+            last_name="Dow",
+            email="johndow@gmail.com",
+            is_staff=True,
+            is_active=False,
+            date_joined=timezone.now(),
+            nick_name="strider",
+            birthday=datetime(1988, 3, 28),
+            gender="male",
+            address="No.7 Euclid St. Mountain View, San Francisco, CA, USA",
+            mobile="550(425)8809",
+            image="media/image/default.png",
+        )
+        jim = UserProfile(
+            username="jim",
+            password="123456",
+            first_name="jim",
+            last_name="Dow",
+            email="jimdoug@gmail.com",
+            is_staff=True,
+            is_active=False,
+            date_joined=timezone.now(),
+            nick_name="bear",
+            birthday=datetime(1988, 3, 28),
+            gender="male",
+            address="No.8 Euclid St. Mountain View, San Francisco, CA, USA",
+            mobile="550(425)3536",
+            image="media/image/default.png",
+        )
+        john.save()
+        jim.save()
+
+        saved_users = UserProfile.objects.all()
+        self.assertEqual(saved_users.count(), 2)
+        self.assertEqual(john.username, saved_users[0].username)
+        self.assertEqual(jim.username, saved_users[1].username)

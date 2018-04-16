b109db2c2d2e1cd480bb351ce4fd2897b8d30120
haochengz <haochengzhao@outlook.com>
2018-04-16 15:56:10 +0800

    编写了organizations模块模型

--- organizations/models.py
+++ organizations/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(verbose_name="城市名", max_length=20)
+    desc = models.CharField(verbose_name="城市描述", max_length=200)
+    add_time = models.DateTimeField(verbose_name="添加时间", default=timezone.now)
+
+    class Meta:
+        verbose_name = "城市"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Org(models.Model):
+    name = models.CharField(verbose_name="机构名称", max_length=50)
+    desc = models.TextField(verbose_name="机构描述")
+    hits = models.IntegerField(verbose_name="点击量", default=0)
+    favorite_nums = models.IntegerField(verbose_name="收藏数", default=0)
+    image = models.ImageField(verbose_name="封面图", upload_to="media/org/%Y/%m")
+    address = models.CharField(verbose_name="地址", max_length=200)
+    located = models.ForeignKey(Location, verbose_name="位于", on_delete=models.CASCADE)
+    add_time = models.DateTimeField(verbose_name="添加时间", default=timezone.now)
+
+    class Meta:
+        verbose_name = "机构"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Instructor(models.Model):
+    name = models.CharField(verbose_name="教师名称", max_length=50)
+    hits = models.IntegerField(verbose_name="点击量", default=0)
+    favorite_nums = models.IntegerField(verbose_name="收藏数", default=0)
+    employed = models.CharField(verbose_name="就职于", max_length=50)
+    position = models.CharField(verbose_name="职位", max_length=50)
+    service_len = models.IntegerField(verbose_name="工作年限", default=0)
+    characteristics = models.CharField(verbose_name="教学特点", max_length=500)
+    add_time = models.DateTimeField(verbose_name="添加时间", default=timezone.now)
+
+    class Meta:
+        verbose_name = "教师"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
--- organizations/tests.py
+++ organizations/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Location, Instructor, Org
+
+
+class OrganizationsModuleDBConnectionTest(TestCase):
+
+    def test_org_saving_and_retrieving_from_db(self):
+        bj, sh = self.get_cities()
+        tsinghua = Org(
+            name="Tsinghua University",
+            desc="A big university",
+            hits=1002332,
+            favorite_nums=89822,
+            image="media/org/2018/04",
+            address="Beside the Peiking University",
+            located=bj,
+        )
+        fudan = Org(
+            name="Fudan University",
+            desc="Another big university",
+            hits=98639,
+            favorite_nums=34810,
+            image="media/org/2018/04",
+            address="Somewhere in ShangHai",
+            located=sh,
+        )
+        tsinghua.save()
+        fudan.save()
+
+        orgs = Org.objects.all()
+        self.assertEqual(orgs.count(), 2)
+        self.assertEqual(orgs[0].name, tsinghua.name)
+        self.assertEqual(orgs[1].name, fudan.name)
+
+    def test_instructor_saving_and_retrieving_from_db(self):
+        pass
+
+    def test_location_saving_and_retrieving_from_db(self):
+        pass
+
+    def get_cities(self):
+        beijing = Location(
+            name="Beijing",
+            desc="Just a city",
+        )
+        shanghai = Location(
+            name="Shanghai",
+            desc="Just another city",
+        )
+        beijing.save()
+        shanghai.save()
+        return beijing, shanghai

e994a133902867dc3ac54921747916d6655c4e8b
haochengz <haochengzhao@outlook.com>
2018-04-17 15:02:28 +0800

    将users模型提交到admin管理

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from .models import UserProfile
+from .models import UserProfile, EmailVerify, Banner
 
 
 class UserProfileAdmin(admin.ModelAdmin):
     pass
 
 
+class EmailVerifyAdmin(admin.ModelAdmin):
+    pass
+
+
+class BannerAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(EmailVerify, EmailVerifyAdmin)
+admin.site.register(Banner, BannerAdmin)
+

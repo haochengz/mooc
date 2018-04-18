a5d739961945bbe091edf233e111b2996d77d6e4
haochengz <haochengzhao@outlook.com>
2018-04-18 17:04:00 +0800

    测试login视图，暂时无法测试用户登陆

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from users.views import index, user_login
+from users.models import UserProfile
 
 
 class IndexViewTest(TestCase):
@@ -21,3 +22,18 @@ class LoginViewTest(TestCase):
     def test_login_url_resolve(self):
         found = resolve('/login/')
         self.assertEqual(found.func, user_login)
+
+    def test_login_url_GET_correct_template(self):
+        resp = self.client.get('/login/')
+        self.assertTemplateUsed(resp, "login.html")
+
+    def test_login_url_POST_to_existing_user(self):
+        user = UserProfile.objects.create(username="test", password="123456abc", email="test@test.com")
+        resp = self.client.post('/login/', data={
+            "username": "test",
+            "password": "123456abc",
+        })
+        # TODO: 不能测试用户登陆
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "index.html")
+        self.assertTrue(resp.wsgi_request.user.is_authenticated)

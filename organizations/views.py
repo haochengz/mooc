d323a0192263cc411c842358c897d48889b4ee03
haochengz <haochengzhao@outlook.com>
2018-04-28 14:44:40 +0800

    sift organizations by org type or locations

--- organizations/views.py
+++ organizations/views.py
@@ -14,12 +14,25 @@ class OrgListView(View):
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
+        city_id = request.GET.get('city', '')
+        category = request.GET.get('ct', '')
         all_cities = Location.objects.all()
         all_orgs = Org.objects.all()
+
+        if city_id:
+            all_orgs = all_orgs.filter(located_id=city_id)
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
         paginator = Paginator(all_orgs, 8, request=request)
         orgs = paginator.page(page)
         return render(request, "org-list.html", {
             "orgs": orgs,
             "all_cities": all_cities,
             "org_nums": all_orgs.count(),
+            "city_id": city_id,
+            "category": category,
         })
+
+    # TODO: test pagination
+    # TODO:

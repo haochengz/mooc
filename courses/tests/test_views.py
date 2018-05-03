30b19a07a8c4f0e3e30bc065776af112659bff39
haochengz <haochengzhao@outlook.com>
2018-05-03 14:52:57 +0800

    pagination of course list page and tests

--- courses/tests/test_views.py
+++ courses/tests/test_views.py
@@ -3,9 +3,80 @@ from django.urls import resolve
 from django.test import TestCase
 
 from courses.views import CourseListView
+from courses.models import Course
+from organizations.models import Org, Location
 
 
 class CourseListViewTest(TestCase):
+
+    def setUp(self):
+        mountain_view = Location.objects.create(
+            name="Mountain View",
+        )
+        stanford = Org.objects.create(
+            name="Stanford University",
+            located=mountain_view,
+        )
+        Course.objects.create(
+            name="Compiler",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Introduction to Database",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Database",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="How to create a website",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Deep learning",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Python",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Programming Methodology",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Math",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Operating System",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
+        Course.objects.create(
+            name="Network",
+            duration_mins=300,
+            hits=1000,
+            org=stanford,
+        )
 
     def test_resolve_url_correct(self):
         found = resolve("/course/list/")
@@ -14,3 +85,22 @@ class CourseListViewTest(TestCase):
     def test_render_correct_template(self):
         resp = self.client.get("/course/list/")
         self.assertTemplateUsed(resp, "course-list.html")
+
+    def test_pagination_of_course_list_page_by_8_courses_each_page(self):
+        resp = self.client.get("/course/list/")
+        self.assertContains(resp, "Database")
+        self.assertNotContains(resp, "Introduction to Database")
+        self.assertContains(resp, "下一页")
+        self.assertNotContains(resp, "上一页")
+
+    def test_by_default_none_page_parameter_returns_first_page(self):
+        resp1 = self.client.get("/course/list/", {
+            "page": 1
+        })
+        resp2 = self.client.get("/course/list/")
+        self.assertEqual(resp1.content, resp2.content)
+
+    def test_by_default_the_list_page_returns_the_course_by_the_order_of_adding_time(self):
+        resp = self.client.get("/course/list/")
+        self.assertContains(resp, "Network")
+        self.assertNotContains(resp, "Compiler")
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 
 from django.views.generic import View
 from django.shortcuts import render
+from pure_pagination import Paginator, PageNotAnInteger
 
 from .models import Course
 
@@ -9,7 +10,14 @@ class CourseListView(View):
 
     @staticmethod
     def get(request):
-        all_courses = Course.objects.all()
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        all_courses = Course.objects.all().order_by("-add_time")
+
+        paginator = Paginator(all_courses, 8, request=request)
+        courses = paginator.page(page)
         return render(request, "course-list.html", {
-            "all_courses": all_courses,
+            "courses": courses,
         })
